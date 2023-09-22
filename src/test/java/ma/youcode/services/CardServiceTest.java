ab37e0a6497702d03ad4b81b64c16f55936e9abe
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-09-22 17:05:01 +0100

    add/ tests added

--- src/test/java/ma/youcode/services/CardServiceTest.java
+++ src/test/java/ma/youcode/services/CardServiceTest.java
@@ -1,4 +1,26 @@
+package ma.youcode.services;
+
 import junit.framework.TestCase;
+
 public class CardServiceTest extends TestCase {
-  
+
+    public void testCreer_cartes() {
+    }
+
+    public void testMelanger_cartes() {
+        CardService service = new CardService();
+        service.tirer_carte(new int[][]{{1,2},{1,3},{1,4}});
+    }
+
+    public void testPiocher_n_cartes() {
+    }
+
+    public void testDefausser_cartes() {
+    }
+
+    public void testTirer_carte() {
+    }
+
+    public void testExtraire_ieme_carte() {
+    }
 }
--- src/test/java/ma/youcode/utils/CollectorTest.java
+++ src/test/java/ma/youcode/utils/CollectorTest.java
@@ -1,4 +1,22 @@
+package ma.youcode.utils;
+
 import junit.framework.TestCase;
+
 public class CollectorTest extends TestCase {
-  
+
+    public void testCollect() {
+        int[][] arr1 = new int[2][];
+        arr1[0] = new int[]{1,2};
+        arr1[1] = new int[]{1,3};
+
+        int[][] arr2 = new int[2][];
+        arr2[0] = new int[]{2,2};
+        arr2[1] = new int[]{2,3};
+
+        int[][] result = Collector.collect(arr1, arr2);
+        assertEquals(4,result.length);
+        //assertEquals(2,result[2][0]);
+        // assertEquals(2,result[2][1]);
+
+    }
 }
