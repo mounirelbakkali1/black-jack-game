d1ab15e3f0f085300971f00808f5793d309e4e77
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-09-22 17:05:50 +0100

    add/ signature modified

--- src/main/java/ma/youcode/services/ICardService.java
+++ src/main/java/ma/youcode/services/ICardService.java
@@ -1,8 +1,22 @@
 package ma.youcode.services;
 
 public interface ICardService {
-    public int[][][] creer_cartes();
-    public int[][][] melanger_cartes(int[][][] cartes);
-    public int[][][] piocher_n_cartes(int[][][] cartes, int n);
-    public void defausser_cartes(int[][][] pioche, int[][][] cartes_a_defausses);
+    public int[][] creer_cartes();
+    public int[][] melanger_cartes(int[][] cartes);
+    public int[][][] piocher_n_cartes(int[][] cartes, int n);
+    public int[][] defausser_cartes(int[][] pioche, int[][] cartes_a_defausses);
+
+    default int getCardValue(int[] card){
+        switch (card[1]){
+            case 1 -> {
+                return 11;
+            }
+            case 11,12,13 -> {
+                return 10;
+            }
+            default -> {
+                return card[1];
+            }
+        }
+    }
 }
--- src/main/java/ma/youcode/services/IGameService.java
+++ src/main/java/ma/youcode/services/IGameService.java
@@ -2,10 +2,13 @@ package ma.youcode.services;
 
 import ma.youcode.models.Gamer;
 
+import java.util.List;
+
 public interface IGameService {
-    public void start(Gamer gamer, String token);
+    public void start(Gamer gamer);
     public void newRound(String token);
 
     public void hit();
     public void stand();
+    public List<String> canPlayWith();
 }
