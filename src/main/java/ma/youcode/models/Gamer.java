d1c33c995264471e6d41d04a64caa7d63bad63d5
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-09-23 18:01:43 +0100

    added solde increase/decrease logic for gamer

--- src/main/java/ma/youcode/models/Gamer.java
+++ src/main/java/ma/youcode/models/Gamer.java
@@ -11,4 +11,33 @@ public class Gamer {
     private int playedRounds;
     private int wonRounds;
     private int lostRounds;
+
+    public void incrementPlayedRounds(){
+        this.playedRounds++;
+    }
+
+    private void incrementWonRounds(){
+        this.wonRounds++;
+    }
+
+    private void incrementLostRounds(){
+        this.lostRounds++;
+    }
+
+    public void decrementSolde(BigInteger solde){
+        this.initialSolde = this.initialSolde.subtract(solde);
+    }
+
+
+    public void incrementSolde(BigInteger solde){
+        this.initialSolde =  this.initialSolde.add(solde);
+    }
+
+    public void wonRound(BigInteger solde){
+        incrementWonRounds();
+        incrementSolde(solde.multiply(BigInteger.valueOf(2)));
+    }
+    public void lostRound(){
+        incrementLostRounds();
+    }
 }
