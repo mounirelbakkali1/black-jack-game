d6973b515899c54bfa7dbf25182d1e2a657780b5
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-09-23 12:35:30 +0100

    add / app entry point

--- src/main/java/ma/youcode/App.java
+++ src/main/java/ma/youcode/App.java
@@ -1,6 +1,8 @@
 package ma.youcode;
 
 
+import ma.youcode.controllers.GameController;
+import ma.youcode.services.GameService;
 import ma.youcode.utils.Printer;
 
 public class App
@@ -8,5 +10,7 @@ public class App
     public static void main( String[] args )
     {
         Printer.into();
+        GameController controller = new GameController(new GameService());
+        controller.start();
     }
 }
